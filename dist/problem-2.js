e2f8a62b564a34903f33caf7339aa7c0f87cbdce
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2024-11-08 00:17:50 +0600

    problem2

---
+++ dist/problem-2.js
@@ -0,0 +1,14 @@
+"use strict";
+function removeDuplicates(inputArray) {
+    let emptyArray = [];
+    for (let i = 0; i < inputArray.length; i++) {
+        if (emptyArray.indexOf(inputArray[i]) === -1) {
+            emptyArray.push(inputArray[i]);
+        }
+    }
+    return emptyArray;
+}
+// Sample Input:
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+// Sample Output:
+// [1, 2, 3, 4, 5]
---
+++ src/problem-2.ts
@@ -0,0 +1,23 @@
+
+function removeDuplicates(inputArray:number[]) {
+
+    let emptyArray : number[]=[];
+
+    for (let i = 0; i < inputArray.length; i++) {
+        if (emptyArray.indexOf(inputArray[i])===-1) {
+            emptyArray.push(inputArray[i])
+        }
+        
+    }
+
+    return emptyArray;
+    
+}
+// Sample Input:
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]))
+
+
+
+// Sample Output:
+// [1, 2, 3, 4, 5]
